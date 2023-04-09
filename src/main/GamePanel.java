6b29794e91a75b8fc633817f14c38155511a1743
GabanNicusor <122691474+GabanNicusor@users.noreply.github.com>
2023-04-09 08:48:37 +0300

    Add files via upload

---
+++ src/main/GamePanel.java
@@ -0,0 +1,43 @@
+package main;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+import static main.Game.GAME_HEIGHT;
+import static main.Game.GAME_WIDTH;
+
+public class GamePanel extends JPanel {
+
+	private MouseInputs mouseInputs;
+	private Game game;
+
+	public GamePanel(Game game) {
+		mouseInputs = new MouseInputs(this);
+		this.game = game;
+		setPanelSize();
+		addKeyListener(new KeyboardInputs(this));
+		addMouseListener(mouseInputs);
+		addMouseMotionListener(mouseInputs);
+	}
+// Set the size for our game window
+	private void setPanelSize() {
+		Dimension size = new Dimension(GAME_WIDTH, GAME_HEIGHT);
+		setPreferredSize(size);
+	}
+
+	public void updateGame() {
+
+	}
+// Access the game function to draw everything through this function 
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		game.render(g);
+	}
+
+	public Game getGame() {
+		return game;
+	}
+
+}
